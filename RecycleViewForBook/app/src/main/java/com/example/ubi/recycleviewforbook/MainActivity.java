0d6eace3b462b8e6ae344fb583c1dd66726adc28
lgct <lt1550919167@gmail.com>
2017-10-18 21:36:52 +0800

    添加侧滑导航菜单

--- RecycleViewForBook/app/src/main/java/com/example/ubi/recycleviewforbook/MainActivity.java
+++ RecycleViewForBook/app/src/main/java/com/example/ubi/recycleviewforbook/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.ubi.recycleviewforbook;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
