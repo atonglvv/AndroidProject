a26f71f9c2f910bc9d42aeffb395086793d3b1cb
lgct <lt1550919167@gmail.com>
2017-10-21 09:10:09 +0800

    Revert "Revert "Modify UserAvatar to Radius""
    This reverts commit 03c8cc94476a2b2f5136cf344b79346107ee31cc.

--- RecycleViewForBook/app/src/main/java/com/example/ubi/activity/MainActivity.java
+++ RecycleViewForBook/app/src/main/java/com/example/ubi/activity/MainActivity.java
@@ -1,10 +1,12 @@
-package com.example.ubi.recycleviewforbook;
+package com.example.ubi.activity;
 
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.view.MenuItem;
+
+import com.example.ubi.recycleviewforbook.R;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
