1634ea34c8473460b5931d9fa596daae2ebd57d4
lgct <lt1550919167@gmail.com>
2017-10-21 16:07:31 +0800

    add ButterKnife dependency

--- RecycleViewForBook/app/src/main/java/com/example/ubi/activity/MainActivity.java
+++ RecycleViewForBook/app/src/main/java/com/example/ubi/activity/MainActivity.java
@@ -4,16 +4,28 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.example.ubi.recycleviewforbook.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+//        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Attitude");
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_drawer_home);
     }
 
     @Override
