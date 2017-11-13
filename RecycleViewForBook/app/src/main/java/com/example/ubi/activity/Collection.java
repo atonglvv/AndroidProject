6a38f7d81034c78db091fd51bc0bc5d18a29a648
lgct <lt1550919167@gmail.com>
2017-11-13 19:58:44 +0800

    Collection Activity

---
+++ RecycleViewForBook/app/src/main/java/com/example/ubi/activity/Collection.java
@@ -0,0 +1,12 @@
+package com.example.ubi.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by LvTong on 2017/11/13 19:48
+ * E-Mail Address：lt1550919167@163.com
+ */
+
+public class Collection extends AppCompatActivity {
+
+}
