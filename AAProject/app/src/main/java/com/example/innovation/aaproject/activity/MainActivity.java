b9f673638571b31c6b3b95fa8f091e1fcaec99cb
lgct <lt1550919167@gmail.com>
2017-11-19 15:13:30 +0800

    CalculateActivity

--- AAProject/app/src/main/java/com/example/innovation/aaproject/activity/MainActivity.java
+++ AAProject/app/src/main/java/com/example/innovation/aaproject/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.innovation.aaproject.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -29,8 +30,10 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this,CalculateActivity.class);
+                startActivity(intent);
             }
         });
 
