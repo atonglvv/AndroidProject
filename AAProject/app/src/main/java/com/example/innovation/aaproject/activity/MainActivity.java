ffe7031f3e8c8a9d5ab0b5d9f7dd0551ca67b9eb
lgct <lt1550919167@gmail.com>
2017-11-22 20:07:26 +0800

    add SettingActivity

--- AAProject/app/src/main/java/com/example/innovation/aaproject/activity/MainActivity.java
+++ AAProject/app/src/main/java/com/example/innovation/aaproject/activity/MainActivity.java
@@ -94,9 +94,8 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+            Intent intent = new Intent(this,SettingActivity.class);
+            startActivity(intent);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
